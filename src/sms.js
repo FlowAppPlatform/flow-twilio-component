da9556fc2a6e30864a94a253743ba41718402891
Jerry Edebua <jeredebua@gmail.com>
2018-09-26 15:49:48 +0300

    wrote tests then sms, component classes

---
+++ src/sms.js
@@ -0,0 +1,50 @@
+/**
+ *
+ * This class constructs an sms message and creates it with th create() method
+ * The class uses Twilio to send sms
+ * 
+ *
+ */
+
+const twilio = require('twilio');
+
+class SMS {
+
+  constructor(ACCOUNT_SID, AUTH_TOKEN, from, to, body) {
+    this.from = from;
+    this.to = to;
+    this.body = body;
+    
+    this.ACCOUNT_SID = ACCOUNT_SID;
+    this.AUTH_TOKEN = AUTH_TOKEN;
+
+    this.twilio = twilio(this.ACCOUNT_SID, this.AUTH_TOKEN);
+  }
+
+  create() {
+    if (!this.smsValid()) return new Error('SMS error, please check \'SMS\' contructor');
+    return (
+      this.twilio
+        .messages
+        .create({
+          from: this.from,
+          body: this.body,
+          to: this.to,
+        })
+    );
+  }
+
+  smsValid() {
+    return (
+      Boolean(this.ACCOUNT_SID) &&
+      Boolean(this.AUTH_TOKEN) &&
+      Boolean(this.twilio) &&
+      Boolean(this.from) &&
+      Boolean(this.to) &&
+      Boolean(this.body)
+    );
+  }
+
+}
+
+module.exports = SMS;
---
+++ tests.js
@@ -0,0 +1,61 @@
+var SMS = require('./src/sms');
+var SendSMSComponent = require('./src/component');
+
+describe('SMS Tests', function () {
+  it(`SMS instance "new SMS()" should not be valid`, function (done) {
+    try {
+      new SMS();
+      done(new Error('Invalid sms instance read valid'));
+    } catch(e) { done() }
+  })
+  it(`SMS instance "new SMS(
+    'ACe4e429f0aeb107130daabe39bffb98de','171eea1f076c27d6ed5bcd48d803abf4',
+    '+18646574367','+15107750208','Body')" should not be valid`, function (done) {
+    const sms = new SMS(
+      'ACe4e429f0aeb107130daabe39bffb98de', '171eea1f076c27d6ed5bcd48d803abf4',
+      '', 'to@sample.com', 'Body');
+    done(!sms.smsValid() ? null : new Error('Invalid sms instance read valid'));
+  })
+  it(`SMS instance "new SMS(
+    'ACe4e429f0aeb107130daabe39bffb98de','',
+    '+18646574367','+15107750208','Body')" should not be valid`, function (done) {
+    try {
+      const sms = new SMS(
+        'ACe4e429f0aeb107130daabe39bffb98de', '',
+        '+18646574367','+15107750208','Body'
+      );
+      done(!sms.smsValid() ? null : new Error('Invalid sms instance read valid'));
+    } catch(e) { done(); }
+  })
+  it(`SMS instance "new SMS(
+    'ACe4e429f0aeb107130daabe39bffb98de','171eea1f076c27d6ed5bcd48d803abf4',
+    '+18646574367','+15107750208','Body')" should be valid`, function (done) {
+    const sms = new SMS(
+      'ACe4e429f0aeb107130daabe39bffb98de', '171eea1f076c27d6ed5bcd48d803abf4',
+      '+18646574367','+15107750208','Body'
+    );
+    done(sms.smsValid() ? null : new Error('Valid sms instance read invalid'));
+  })
+})
+
+describe('Component Tests', function () {
+  it('Component should have all required properties', function (done) {
+    try {
+      const component = new SendSMSComponent();
+      component.getProperty('ACCOUNT_SID');
+      component.getProperty('AUTH_TOKEN');
+      component.getProperty('From');
+      component.getProperty('To');
+      component.getProperty('Body');
+      done();
+    } catch(e) { done(new Error('Component missing required properties')); }
+  })
+  it('Component should have all required ports', function (done) {
+    try {
+      const component = new SendSMSComponent();
+      component.getPort('Sent');
+      component.getPort('Error');
+      done();
+    } catch(e) { done(new Error('Component missing required ports')); }
+  })
+})
